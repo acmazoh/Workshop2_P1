09fedfe777811761c2054963fa947ccef678ec4c
acmazoh149-crypto <acmazoh149@gmail.com>
2026-03-02 16:49:32 -0500

    finalización taller #2

--- movie/models.py
+++ movie/models.py
@@ -4,6 +4,11 @@ from django.db import models
 
 class Movie(models.Model): 
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250) 
+    description = models.CharField(max_length=250, null=True, blank=True) 
     image = models.ImageField(upload_to='movie/images/') 
     url = models.URLField(blank=True)
+    genre = models.CharField(blank=True, max_length=250)
+    year = models.IntegerField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
