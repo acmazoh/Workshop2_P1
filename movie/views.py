2f733717e83299449dcb9d0ecbb766dd74b55369
acmazoh149-crypto <acmazoh149@gmail.com>
2026-03-04 08:13:54 -0500

    Agregar grafica de pelicula por genero

--- movie/views.py
+++ movie/views.py
@@ -61,6 +61,46 @@ def statistics_view(request):
     # Renderizar la plantilla statistics.html con la gráfica
     return render(request, 'statistics.html', {'graphic': graphic})
 
+def statistics2_view(request):
+    matplotlib.use('Agg')
+    all_genres = Movie.objects.values_list('genre', flat=True)
+    movie_counts_by_genre = {}
+    
+    for genre_field in all_genres:
+        if genre_field:
+            
+            first_genre = genre_field.split(',')[0].strip()  
+        else:
+            first_genre = "None"
+        
+        if first_genre in movie_counts_by_genre:
+            movie_counts_by_genre[first_genre] += 1
+        else:
+            movie_counts_by_genre[first_genre] = 1
+    
+    movie_counts_by_genre = dict(sorted(movie_counts_by_genre.items()))
+    
+    bar_width = 0.5
+    bar_positions = range(len(movie_counts_by_genre))
+    
+    plt.bar(bar_positions, movie_counts_by_genre.values(), width=bar_width, align='center')
+    plt.title('Movies per genre')
+    plt.xlabel('Genre')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_genre.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+    
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png).decode('utf-8')
+    
+    return render(request, 'statistics2.html', {'graphic': graphic})
+
 def signup(request):
     email = request.GET.get('email')
     return render(request,'signup.html',{'email': email})
